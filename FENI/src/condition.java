f5ef26e4757a415ebb7c316bd09be443b0158cd4
feninisar <44470964+feninisar@users.noreply.github.com>
2021-09-28 11:15:46 -0700

    Add files via upload

---
+++ FENI/src/condition.java
@@ -0,0 +1,10 @@
+public class condition {
+    public static void main(String[] args) {
+        int a = 15;
+        if (a > 20)
+            System.out.println("a is greater than 10");
+        else
+            System.out.println("a is less than 10");
+
+    }
+}
